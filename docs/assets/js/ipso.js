259e374c2bdd52daa4ad9ecfbf432c526af1929a
CogDog <cogdogblog@gmail.com>
2017-03-19 14:16:47 -0700

    Javascript with comments

--- docs/assets/js/ipso.js
+++ docs/assets/js/ipso.js
@@ -1,25 +1,25 @@
 function get_sum( lines, paragraphs ) {
-
+	// a place to put the generated stuff
 	var stuff = '';
 
+	// loop over the number of desired paragraphs
 	for (i = 0; i < paragraphs; i++) { 
 	
-	stuff += '<p>';
-	
+		// open that paragraph
+		stuff += '<p>';
+		// now loop for the number of desired sentences
 		for (j = 0; j < lines; j++) { 
+		
+			// append a random line from the source array
 			stuff +=  all_lines[Math.floor(Math.random()*all_lines.length)];
 		}
 		
-	stuff += '</p>';
+		// close that paragraph
+		stuff += '</p>';
 	}
-
+	
+	// update the div in our page
 	$("#lorem").html(stuff);
-}
-
-function checkSubmit(e) {
-   if(e && e.keyCode == 13) {
-      document.forms[0].submit();
-   }
 }
 
 // A $( document ).ready() block.
@@ -27,6 +27,9 @@ $( document ).ready(function() {
     get_sum( $("#sent").val(), $("#para").val() );
     
     $( ".ipso" ).change(function() {
+    
+    	// do a little bounds check on the form input, and make sure
+    	// we have a number
     
     	if ( $(this).val() > 10) $(this).val(10);
     	if ( $(this).val() < 1) $(this).val(1);
